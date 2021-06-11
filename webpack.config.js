64ce83a262576e914399717a663892cdaaca18da
aluba0609 <164868966@qq.com>
2021-06-11 23:46:36 +0800

    弱鸡webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports={
         index:'./src/index.js'
     },
     output:{
-        filename:'[bundle].js',
+        filename:'bundle.js',
         path:path.resolve(__dirname,'dist')
     },
     // module:{
